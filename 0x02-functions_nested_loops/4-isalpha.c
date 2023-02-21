c4ed1dbdb60273433965496777a980dc0b483656
siska <special@gmail.com>
2023-02-21 13:30:48 -0800

    4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*_isalpha -check if a character is lowercase or uppercase.
+*@c: The character to be checked.
+*
+* Return: 1 if character is lowercase or uppercase, or otherwise.
+*/
+
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
+}
