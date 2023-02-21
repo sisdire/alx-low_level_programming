1600b1b6167f592df1c2db4af83a51e19a523fa4
siska <special@gmail.com>
2023-02-21 13:09:59 -0800

    2-print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - print alphabet 10 times, in lowercase.
+*/
+
+void print_alphabet_x10(void)
+{
+	int coun = 0;
+	char alpha;
+
+	while (coun++ <= 9)
+	{
+		for (alpha = 97; alpha <= 122; alpha++)
+			_putchar(alpha);
+		_putchar('\n');
+	}
+}
