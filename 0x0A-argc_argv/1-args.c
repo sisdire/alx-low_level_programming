38d905caf1fe2da53292a34365f1eacb5b5c20c7
siska <special@gmail.com>
2023-03-10 11:32:32 -0800

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,19 +1,17 @@
-#include <stdio.h>                                                                                                                   
-#include "main.h"                                                                                                                    
-                                                                                                                                     
-/**                                                                                                                                  
- * main - print the name of the program                                                                                              
- * @argc: Count arguments                                                                                                            
- * @argv: Arguments                                                                                                                  
- *                                                                                                                                   
- * Return: Always 0 (Success)                                                                                                        
- */                                                                                                                                  
-                                                                                                                                     
-int main(int argc, char *argv[])                                                                                                     
-{                                                                                                                                    
-(void) argv; /*Ignore argv*/                                                                                                         
-printf("%i\n", argc - 1);                                                                                                            
-                                                                                                                                     
-return (0);                                                                                                                          
-}
+#include "main.h"
 
+/**
+ * main - the entry point
+ * @argc: argument counter
+ * @argv: array of pointer to strings
+ *
+ * Return: 0
+ */
+
+
+int main(int argc, char *argv[] __attribute__ ((unused)))
+
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
