0a92621676041e2842a981bc8ffc7f31bd0898d6
sisay <sis7242@gmail.com>
2023-03-30 11:46:45 -0700

    1-list_len

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,21 +1,18 @@
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked list
- * @h: pointer to the list_t list
+ * list_len - finds the number of elements in a linked list
+ * @h: pointer to the start of the linked list
  *
- * Return: number of elements in h
+ * Return: number of elements in the linked list
  */
 size_t list_len(const list_t *h)
 {
- size_t n = 0;
+	const list_t *tmp;
+	unsigned int i;
 
- while (h)
- {
- n++;
- h = h->next;
- }
- return (n);
+	tmp = h;
+	for (i = 0; tmp; i++)
+		tmp = tmp->next;
+	return (i);
 }
-
