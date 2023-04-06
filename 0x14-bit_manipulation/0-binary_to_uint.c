c292b9b2f5176e96b5e3b2fd19e094c63438eacb
sisay <sis7242@gmail.com>
2023-04-06 14:19:06 -0700

    0-binary_to_uint updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,19 +9,18 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int j;
+	unsigned int dec_val = 0;
 
-	j = 0;
 	if (!b)
 		return (0);
-	for (i = 0; b[i] != '\0'; i++)
+
+	for (i = 0; b[i]; i++)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-	for (i = 0; b[i] != '\0'; i++)
-	{
-		j <<= 1;
-		if (b[i] == '1')
-			j += 1;
-	}
+
+	return (dec_val);
+}
+
